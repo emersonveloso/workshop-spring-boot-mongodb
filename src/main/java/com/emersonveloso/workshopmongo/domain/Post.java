c428ecc26dcb031e2c30e0bcb0834e1222361aef
Emerson Veloso <emesonveloso.s@gmail.com>
2023-01-27 20:10:47 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/emersonveloso/workshopmongo/domain/Post.java
+++ src/main/java/com/emersonveloso/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.emersonveloso.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.emersonveloso.workshopmongo.dto.AuthorDTO;
+import com.emersonveloso.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> commets = new ArrayList<>();
 
     public Post() {
 
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getCommets() {
+        return commets;
+    }
+
+    public void setCommets(List<CommentDTO> commets) {
+        this.commets = commets;
     }
 
     @Override
